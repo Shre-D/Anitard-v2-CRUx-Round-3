f77fff31ad640cb95651635ae9e0c8670c83fe53
Shre-D <shreyash1264@gmail.com>
2023-03-07 14:47:37 +0530

    updated sorting and filtering methods

--- components/PrepareHomeAnime.tsx
+++ components/PrepareHomeAnime.tsx
@@ -1,7 +1,7 @@
 import SearchCardAnime from "./SearchCardAnime";
 import { Animetype } from "../types";
 import { ZoroType } from "./HomeDisplay";
-import { useState,useEffect } from "react";
+import { useState,useEffect, use } from "react";
 
 interface Props {
   animeList: Animetype[];
@@ -9,34 +9,32 @@ interface Props {
 }
 
 function PrepareHomeAnime({ animeList, searched }: Props) {
-  
-  const [sort, setSort] = useState("Score");
-
-  const scoreList=animeList
-  const popularityList=animeList
-  const episodeList=animeList
-  const yearList=animeList
+  const [list,setList]=useState<Animetype[]|undefined>(animeList)  
+  animeList = animeList.filter(((i:any) => i.popularity!=0 ));
+  animeList = animeList.filter(((i:any) => i.score!=0 ));
+  animeList = animeList.filter(((i:any) => i.episodes!=0 ));
+  animeList = animeList.filter(((i:any) => i.aired.from?.slice(0, 4) as number!=0));
+  animeList = animeList.filter(((i:any) => i.popularity!=null ));
+  animeList = animeList.filter(((i:any) => i.score!=null ));
+  animeList = animeList.filter(((i:any) => i.aired.from?.slice(0, 4) as number!=null ));
 
   function sortByPopularity(animeList: any) {
     animeList.sort(function(a: any, b: any) {
-      if ((a.popularity != 0) && (b.popularity != 0)) {
         if (a.popularity > b.popularity) {
           return 1;
         }
         if (a.popularity < b.popularity) {
           return -1;
         }
-    return 0;
-
-      }
-    }
+      return 0
+  }
     );
   }
   function sortByReleaseYear(animeList: any) {
     animeList.sort(function(a: any, b: any) {
       if (
-        (a.aired.from?.slice(0, 4) as number) != null &&
-        (b.aired.from?.slice(0, 4) as number) != null
+        (a.aired.from?.slice(0, 4) as number) != null||0 &&
+        (b.aired.from?.slice(0, 4) as number) != null||0
       ) {
         if (
           (a.aired.from?.slice(0, 4) as number) >
@@ -82,31 +80,19 @@ function PrepareHomeAnime({ animeList, searched }: Props) {
         }
         return 0
   }});
-  }
+  }  
 
+  let scoreList=[...animeList]
+  let popularityList=[...animeList]
+  let episodeList=[...animeList]
+  let yearList=[...animeList]
 
- function Sort(){
-  console.log(sort);
+  sortByScore(scoreList)  
+  sortByPopularity(popularityList)
+  console.log(scoreList);
   
-  if(sort=="Score"){
-    sortByScore(animeList)
-    console.log(list);
-  }
-  if(sort=="Episodes"){
-    sortByEpisodes(animeList)
-    console.log(list);    
-  }
-  if(sort=="Popularity"){
-    sortByPopularity(animeList)
-    console.log(list);    
-  }
-  if(sort=="Release Year"){
-    sortByReleaseYear(animeList)
-    console.log(list);
-  }      
- }
- 
-  const [list,setList]=useState<Animetype[]|undefined>(popularityList) 
+  sortByEpisodes(episodeList)
+  sortByReleaseYear(yearList)   
 
   return (
     <div className="mr-20">
@@ -121,14 +107,22 @@ function PrepareHomeAnime({ animeList, searched }: Props) {
               <select
                 className="w-28 ml-2 p-1 rounded bg-slate-500 text-sm"
                 name="sort"
-                value={sort}
                 onChange={(e) => {
-                  setSort(e.target.value)  
-                  console.log(e.target.value);
-                  Sort()               
+                  if(e.target.value==="Popularity"){
+                    setList(popularityList)
+                  }    
+                  if(e.target.value==="Score"){
+                    setList(scoreList)
+                  }
+                  if(e.target.value==="Release Year"){
+                    setList(yearList)
+                  }
+                  if(e.target.value==="Episodes"){
+                    setList(episodeList)
+                  }       
+                  
                 }}
-              >              
-                  <option value=""></option>  
+              >               
                   <option value="Score">Score</option>
                   <option value="Popularity">Popularity</option>
                   <option value="Release Year">Release Year</option>
@@ -141,7 +135,7 @@ function PrepareHomeAnime({ animeList, searched }: Props) {
         )}
       </div>
       <div className="pl-2 pt-5 pb-20 items-center max-h-80 max-w-64 space-x-0.5 grid grid-cols-3 gap-4 md:space-x-2.5 md:p-2">
-        {animeList?.map((anime: any) => (
+        {list?.map((anime: any) => (
           <SearchCardAnime
             anime={anime}
             searched={searched}
