6c7c673d71a6b204e8253bdff32250f8d9da2213
Shre-D <shreyash1264@gmail.com>
2023-03-07 15:34:39 +0530

    logic fixed

--- components/PrepareHomeAnime.tsx
+++ components/PrepareHomeAnime.tsx
@@ -16,6 +16,7 @@ function PrepareHomeAnime({ animeList, searched }: Props) {
   animeList = animeList.filter(((i:any) => i.aired.from?.slice(0, 4) as number!=0));
   animeList = animeList.filter(((i:any) => i.popularity!=null ));
   animeList = animeList.filter(((i:any) => i.score!=null ));
+  
   animeList = animeList.filter(((i:any) => i.aired.from?.slice(0, 4) as number!=null ));
 
   function sortByPopularity(animeList: any) {
