e7be706e2490d33e84e2ae66dc1ebcb04f06fb97
Shre-D <shreyash1264@gmail.com>
2023-03-19 21:12:24 +0530

    added exceptions

--- components/exceptions.ts
+++ components/exceptions.ts
@@ -7,6 +7,21 @@ const exceptions=
     {name:{falsy:"bakuman3",truthy:"neon-genesis-evangelion-"}},
     {name:{falsy:"shingeki-no-kyojin-the-final-season-kanketsu-hen-zenpen",
            truthy:"shingeki-no-kyojin-the-final-season-kanketsu-hen" 
+    }},
+    {name:{falsy:"boku-no-hero-academia-2",
+           truthy:"boku-no-hero-academia-2nd-season" 
+    }},
+    {name:{falsy:"boku-no-hero-academia-3",
+           truthy:"boku-no-hero-academia-3rd-season" 
+    }},
+    {name:{falsy:"boku-no-hero-academia-4",
+           truthy:"boku-no-hero-academia-4th-season" 
+    }},
+    {name:{falsy:"boku-no-hero-academia-5",
+           truthy:"boku-no-hero-academia-5th-season" 
+    }},
+    {name:{falsy:"boku-no-hero-academia-6",
+           truthy:"boku-no-hero-academia-6th-season" 
     }}
     ]
 
